18ffc0867addb61c7231cd4134f5c8b0ccbdb1d8
Thomas Chappel <thomas.chappel4@gmail.com>
2019-11-28 11:20:10 +0100

    Updates

---
+++ precache-manifest.817abbe1f547040827f6f46d29c82470.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6253a370fd6299cd28a8b61f0aff4cf8",
+    "url": "/ebay-task/index.html"
+  },
+  {
+    "revision": "729ddc5b5c95ee3f0a86",
+    "url": "/ebay-task/static/css/main.5acb69ce.chunk.css"
+  },
+  {
+    "revision": "f2d09bb3456ebe9ade2c",
+    "url": "/ebay-task/static/js/2.83d9b246.chunk.js"
+  },
+  {
+    "revision": "729ddc5b5c95ee3f0a86",
+    "url": "/ebay-task/static/js/main.06273c32.chunk.js"
+  },
+  {
+    "revision": "5dd086a3249c15b367ee",
+    "url": "/ebay-task/static/js/runtime-main.7b81172a.js"
+  }
+]);
